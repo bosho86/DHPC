12f8f5bdb5fa1ee8e5f13fef9bf2fbe9f4f3407d
bosho86 <paudi18@gmail.com>
2019-10-18 15:30:41 +0200

    src

---
+++ src/bellmanfordalgo.cpp
@@ -0,0 +1,17 @@
+#include "bellmanfordalgo.h"
+#include <studio.h>
+#include <math.h>
+#include <stdlib.h>
+
+class bellmanfordalgo{
+public:
+  
+private:
+
+  
+};
+
+void main (){
+
+  return 0;
+}
