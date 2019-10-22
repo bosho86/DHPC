ad1abb130091a3730df9cfcf12fcdaf05f67c36e
claudio <claudioc@student.ethz.ch>
2019-10-22 13:31:00 +0200

    added dijkstra.cpp and dijkstra.hpp

---
+++ include/dijkstra.hpp
@@ -0,0 +1,20 @@
+#ifndef DIJKSTRA_HPP
+#define DIJKSTRA_HPP
+
+template <typename Vector, typename Graph, typename Edge>
+Vector dijkstra(Graph graph, Edge start);
+
+/* PRECONDITIONS:
+ *
+ * 'graph' a graph with non-negative edge weight
+ *
+ * 'start' a vertex element of graph
+ *
+ *
+ * POSTCONDITIONS:
+ *
+ * the return value will calculate the minimal weights of the shortest paths with start arr
+ *
+ */
+
+#endif
---
+++ src/dijkstra.cpp
@@ -0,0 +1,4 @@
+#include "../../dijkstra.hpp"
+
+template <typename Vector, typename Graph, typename Edge>
+Vector dijkstra(Graph graph, Edge start);
